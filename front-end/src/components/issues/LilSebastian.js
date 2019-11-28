fbede443c3e94d7393f353d9d9990d2d732baaa6
github username <github email address>
2019-11-28 13:46:32 +0100

    embedded lil sebastian video

--- front-end/src/components/issues/LilSebastian.js
+++ front-end/src/components/issues/LilSebastian.js
@@ -2,48 +2,58 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const LilSebastian = () => {
-  return <a src='https://i.imgur.com/1f5zHM9.jpg' />
-
-  //  https://www.youtube.com/watch?v=ZbUf9mnm5TA - 5000 Candles in the wind
-  // 5000 Candles in the Wind
-  // Mouse Rat
-  // Album 5000 Candles In The Wind
-
-  // [Verse 1]
-  // Up in horsey heaven, here's the thing
-  // You trade your legs for angels wings
-  // And once we’ve all said good-bye
-  // You take a running leap and you learn to fly
-
-  // [Chorus]
-  // Bye bye Li'l Sebastian
-  // Miss you in the saddest fashion
-  // Bye bye Li'l Sebastian
-  // You’re 5000 candles in the wind
-
-  // [Verse 2]
-  // And though we all miss you everyday
-  // We know you're up there eating heaven's hay
-  // And here's the part that hurts the most
-  // Humans cannot ride a ghost
-
-  // [Chorus]
-  // Bye bye Li'l Sebastian
-  // Miss you in the saddest fashion
-  // Bye bye Li'l Sebastian
-  // You’re 5000 candles in the wind
-
-  // Everybody sing it now!
-  // Bye bye Li'l Sebastian
-  // Miss you in the saddest fashion
-  // Bye bye Li'l Sebastian
-  // You’re 5000 candles in the wind
-
-  // [Outro]
-  // Maybe someday we'll saddle up again
-  // And I know I'll always miss my horsiest friend
-  // Spread your wings and fly
-  // Spread your wings and fly
+  return (
+    <div className='issue-page'>
+      <div className='youtube-block-grid'>
+        <div className='youtube'>
+          <iframe
+            title='5000 Candles in the Wind'
+            width='100%'
+            height='580'
+            src='https://www.youtube.com/embed/ZbUf9mnm5TA'
+            frameBorder='0'
+            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+            allowfullscreen
+          />
+        </div>
+      </div>
+      <div>
+        <h3>
+          5000 Candles in the Wind Mouse Rat Album 5000 Candles In The Wind
+        </h3>
+        <p>Up in horsey heaven, here's the thing </p>
+        <p>You trade your legs for angels wings </p>
+        <p> And once we’ve all said good-bye </p>
+        <p> You take a running leap and you learn to fly</p>
+        <br />
+        <p>Bye bye Li'l Sebastian </p>
+        <p> Miss you in the saddest fashion </p>
+        <p> Bye bye Li'l Sebastian </p>
+        <p> You’re 5000 candles in the wind</p>
+        <br />
+        <p>And though we all miss you everyday </p>
+        <p> We know you're up there eating heaven's hay </p>
+        <p> And here's the part that hurts the most </p>
+        <p> Humans cannot ride a ghost</p>
+        <br />
+        <p>Bye bye Li'l Sebastian </p>
+        <p> Miss you in the saddest fashion </p>
+        <p> Bye bye Li'l Sebastian </p>
+        <p> You’re 5000 candles in the wind</p>
+        <br />
+        <p>Everybody sing it now! </p>
+        <p> Bye bye Li'l Sebastian </p>
+        <p> Miss you in the saddest fashion </p>
+        <p> Bye bye Li'l Sebastian </p>
+        <p> You’re 5000 candles in the wind</p>
+        <br />
+        <p>Maybe someday we'll saddle up again </p>
+        <p> And I know I'll always miss my horsiest friend </p>
+        <p> Spread your wings and fly </p>
+        <p> Spread your wings and fly</p>
+      </div>
+    </div>
+  )
 }
 
 export default LilSebastian
