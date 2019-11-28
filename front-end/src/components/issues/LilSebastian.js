756df0394f1fb30a320a8bd935e184514cf78e18
github username <github email address>
2019-11-28 09:47:06 +0100

    5000 Candles in the Wind

--- front-end/src/components/issues/LilSebastian.js
+++ front-end/src/components/issues/LilSebastian.js
@@ -3,6 +3,47 @@ import { Link } from 'react-router-dom'
 
 const LilSebastian = () => {
   return <a src='https://i.imgur.com/1f5zHM9.jpg' />
+
+  //  https://www.youtube.com/watch?v=ZbUf9mnm5TA - 5000 Candles in the wind
+  // 5000 Candles in the Wind
+  // Mouse Rat
+  // Album 5000 Candles In The Wind
+
+  // [Verse 1]
+  // Up in horsey heaven, here's the thing
+  // You trade your legs for angels wings
+  // And once we’ve all said good-bye
+  // You take a running leap and you learn to fly
+
+  // [Chorus]
+  // Bye bye Li'l Sebastian
+  // Miss you in the saddest fashion
+  // Bye bye Li'l Sebastian
+  // You’re 5000 candles in the wind
+
+  // [Verse 2]
+  // And though we all miss you everyday
+  // We know you're up there eating heaven's hay
+  // And here's the part that hurts the most
+  // Humans cannot ride a ghost
+
+  // [Chorus]
+  // Bye bye Li'l Sebastian
+  // Miss you in the saddest fashion
+  // Bye bye Li'l Sebastian
+  // You’re 5000 candles in the wind
+
+  // Everybody sing it now!
+  // Bye bye Li'l Sebastian
+  // Miss you in the saddest fashion
+  // Bye bye Li'l Sebastian
+  // You’re 5000 candles in the wind
+
+  // [Outro]
+  // Maybe someday we'll saddle up again
+  // And I know I'll always miss my horsiest friend
+  // Spread your wings and fly
+  // Spread your wings and fly
 }
 
 export default LilSebastian
