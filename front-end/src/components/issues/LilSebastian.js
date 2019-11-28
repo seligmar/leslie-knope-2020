626ef83c00f1577f48855ee1ec6e0672390ba353
github username <github email address>
2019-11-28 14:24:38 +0100

    5000 Candles in the Wind divided out

--- front-end/src/components/issues/LilSebastian.js
+++ front-end/src/components/issues/LilSebastian.js
@@ -18,39 +18,41 @@ const LilSebastian = () => {
         </div>
       </div>
       <div>
-        <h3>
-          5000 Candles in the Wind Mouse Rat Album 5000 Candles In The Wind
-        </h3>
-        <p>Up in horsey heaven, here's the thing </p>
-        <p>You trade your legs for angels wings </p>
-        <p> And once we’ve all said good-bye </p>
-        <p> You take a running leap and you learn to fly</p>
-        <br />
-        <p>Bye bye Li'l Sebastian </p>
-        <p> Miss you in the saddest fashion </p>
-        <p> Bye bye Li'l Sebastian </p>
-        <p> You’re 5000 candles in the wind</p>
-        <br />
-        <p>And though we all miss you everyday </p>
-        <p> We know you're up there eating heaven's hay </p>
-        <p> And here's the part that hurts the most </p>
-        <p> Humans cannot ride a ghost</p>
-        <br />
-        <p>Bye bye Li'l Sebastian </p>
-        <p> Miss you in the saddest fashion </p>
-        <p> Bye bye Li'l Sebastian </p>
-        <p> You’re 5000 candles in the wind</p>
-        <br />
-        <p>Everybody sing it now! </p>
-        <p> Bye bye Li'l Sebastian </p>
-        <p> Miss you in the saddest fashion </p>
-        <p> Bye bye Li'l Sebastian </p>
-        <p> You’re 5000 candles in the wind</p>
-        <br />
-        <p>Maybe someday we'll saddle up again </p>
-        <p> And I know I'll always miss my horsiest friend </p>
-        <p> Spread your wings and fly </p>
-        <p> Spread your wings and fly</p>
+        <h3>5000 Candles in the Wind </h3>
+        <h3> Mouse Rat </h3>
+        <h3> 5000 Candles In The Wind</h3>
+        <div style={{ 'font-size': '15px' }}>
+          <div>Up in horsey heaven, here's the thing </div>
+          <div>You trade your legs for angels wings </div>
+          <div> And once we’ve all said good-bye </div>
+          <div> You take a running leap and you learn to fly</div>
+          <br />
+          <div>Bye bye Li'l Sebastian </div>
+          <div> Miss you in the saddest fashion </div>
+          <div> Bye bye Li'l Sebastian </div>
+          <div> You’re 5000 candles in the wind</div>
+          <br />
+          <div>And though we all miss you everyday </div>
+          <div> We know you're up there eating heaven's hay </div>
+          <div> And here's the part that hurts the most </div>
+          <div> Humans cannot ride a ghost</div>
+          <br />
+          <div>Bye bye Li'l Sebastian </div>
+          <div> Miss you in the saddest fashion </div>
+          <div> Bye bye Li'l Sebastian </div>
+          <div> You’re 5000 candles in the wind</div>
+          <br />
+          <div>Everybody sing it now! </div>
+          <div> Bye bye Li'l Sebastian </div>
+          <div> Miss you in the saddest fashion </div>
+          <div> Bye bye Li'l Sebastian </div>
+          <div> You’re 5000 candles in the wind</div>
+          <br />
+          <div>Maybe someday we'll saddle up again </div>
+          <div> And I know I'll always miss my horsiest friend </div>
+          <div> Spread your wings and fly </div>
+          <div> Spread your wings and fly</div>
+        </div>
       </div>
     </div>
   )
