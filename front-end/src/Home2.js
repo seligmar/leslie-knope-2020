3d7754790e35f92a5386ddf8ade9d5d21bb571c1
github username <github email address>
2019-12-09 18:01:45 +0100

    added css

--- front-end/src/Home2.js
+++ front-end/src/Home2.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Home2.css'
 import ReactPlayer from 'react-player'
 import { TwitterTimelineEmbed } from 'react-twitter-embed'
 
-class Home extends React.Component {
+class Home2 extends React.Component {
   render () {
     return (
       <div>
@@ -12,43 +13,13 @@ class Home extends React.Component {
             <h1 className='giving-text'>Give Now</h1>
           </div>
         </Link>
-        <div className='video-wrapper'>
-          <ReactPlayer
-            className='react-player'
-            url='https://media.giphy.com/media/xRfxdozZr2zYI/giphy.mp4'
-            playing='true'
-            loop='true'
-            width='100%'
-            height='70%'
-          />
-        </div>
-        <div className='twitter'>
-          <div className='twitter-box'>
-            <TwitterTimelineEmbed
-              sourceType='profile'
-              screenName='EagletonHater'
-              options={{
-                height: 400,
-                width: 1200
-              }}
-            />
-          </div>
-        </div>
-        <div className='youtube-block-grid'>
-          <div className='youtube'>
-            <iframe
-              width='560'
-              height='315'
-              src='https://www.youtube.com/embed/Xcdhpn-DXTA'
-              frameborder='0'
-              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-              allowfullscreen
-            />
-          </div>
+        <div class='banner'>
+          <p class='h1 display-4'>Find Your Coffee Cup</p>
+          <p class='lead'>We have them all in one place!</p>
         </div>
       </div>
     )
   }
 }
 
-export default Home
+export default Home2
