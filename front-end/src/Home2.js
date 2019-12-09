b91dca31f8749613b5fd021a69bd1d4b565e292b
github username <github email address>
2019-12-09 17:56:43 +0100

    created second home page option

---
+++ front-end/src/Home2.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ReactPlayer from 'react-player'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
+
+class Home extends React.Component {
+  render () {
+    return (
+      <div>
+        <Link to='/donate/leslieknope2020' style={{ textDecoration: 'none' }}>
+          <div className='donationsBar'>
+            <h1 className='giving-text'>Give Now</h1>
+          </div>
+        </Link>
+        <div className='video-wrapper'>
+          <ReactPlayer
+            className='react-player'
+            url='https://media.giphy.com/media/xRfxdozZr2zYI/giphy.mp4'
+            playing='true'
+            loop='true'
+            width='100%'
+            height='70%'
+          />
+        </div>
+        <div className='twitter'>
+          <div className='twitter-box'>
+            <TwitterTimelineEmbed
+              sourceType='profile'
+              screenName='EagletonHater'
+              options={{
+                height: 400,
+                width: 1200
+              }}
+            />
+          </div>
+        </div>
+        <div className='youtube-block-grid'>
+          <div className='youtube'>
+            <iframe
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/Xcdhpn-DXTA'
+              frameborder='0'
+              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              allowfullscreen
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
