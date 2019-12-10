c73a454c7d5db6b1f80f48392bf88a523dd1cd0b
github username <github email address>
2019-12-10 20:56:49 +0100

    began creating user routes and controller

---
+++ back-end/app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def signin
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password]) || user = get_current_user 
+      render json: { username: user.username, token: issue_token({id: user.id})}  
+    else 
+      render json: {error: 'username and password combination invalid'}, status: 401
+    end 
+  end
+
+  private
+
+  def score_params
+      params.require(:user).permit(:username, :highScore)
+  end
+
+end
