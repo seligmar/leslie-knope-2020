59a76fc5eb449bbe1d96cc1aa8278098168e6247
github username <github email address>
2020-02-27 16:30:24 +0100

    created routes and new event function in back end

--- back-end/app/controllers/events_controller.rb
+++ back-end/app/controllers/events_controller.rb
@@ -20,16 +20,25 @@ class EventsController < ApplicationController
 end
 
   def new 
-    new_event = Event.new(title: params[:title], street_address: params[:street_address_1]
-    )
+    new_event = Event.new(title: params[:title], street_address: params[:street_address_1], 
+    city: params[:city], state: params[:state], zip: params[:zip], 
+    datetime: params[:datetime], lat: params[:lat], lng: params[:lng])
+    if new_event.save
+      render json: { title: new_event.title,  
+      datetime: new_event.datetime, city: new_event.city, street_address: new_event.street_address, 
+      state: new_event.state, zip: new_event.zip, lat: new_event.lat, lng: new_event.lng
+    }, status: :create 
+    else 
+      render json: { error: new_event.errors.full_messages }, status: :unprocessable_entity
+    end 
   end 
 
   private 
 
    def event_params 
-     params.require(:event).permit(:title, :start_time, 
+     params.require(:event).permit(:title, :datetime, 
       :street_address_1, :city, :state, :zip, 
-      :datetime, lat, :lng)
+      lat, :lng)
    end 
 
 end
