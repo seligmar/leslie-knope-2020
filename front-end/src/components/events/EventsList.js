6cde5104af0c431619a00656beca879834ec59c0
github username <github email address>
2020-03-21 15:56:23 +0000

    changes to event list card

--- front-end/src/components/events/EventsList.js
+++ front-end/src/components/events/EventsList.js
@@ -9,26 +9,25 @@ const EventsList = props => {
           <div className='event-div'>
             <div className='ui fluid card'>
               <div className='class-content'>
-                <p class='header'>{event.title}</p>
                 <img
                   className='ui small right floated image'
                   alt='Warren Yard Sign'
                   src='
                   https://cdn.shopify.com/s/files/1/0250/1217/4900/products/p_r_pawneegoddessesbadge_mens_shortsleeve_tshirt_rollover_7_1200x630.jpg?v=1572164280                  '
                 />
+                <p class='header'>{event.title}</p>
                 <p>
                   <p> on </p>
                   {event.month} {event.day}, {event.year}
                 </p>
                 <p>
-                  <p> Starting At: </p>
+                  <p>at</p>
                   {event.time} {event.timestamp}
                 </p>
-                {/*  - {event.start_time} on {event.day}/{event.month}/{event.year}</p> */}
-                <p class='description'>{event.street_address}</p>
-                <p class='description'>
+                <div class='description'>{event.street_address}</div>
+                <div class='description'>
                   {event.city}, {event.state} {event.zip}
-                </p>
+                </div>
                 {/* <button className='event-button' onClick={(e) => props.rsvp(e, event.id)}>RSVP!</button> */}
               </div>
             </div>
